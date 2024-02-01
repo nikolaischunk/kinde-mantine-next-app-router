2c9ecec43bb3162abde75957138067f1fb10fdfa
Nikolai Schunk <58025175+nikolaischunk@users.noreply.github.com>
2024-02-01 15:56:08 +0100

    add signin button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { LoginLink } from '@kinde-oss/kinde-auth-nextjs/components';
+import { Button } from '@mantine/core';
+
 export default function HomePage() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">HomePage</h1>
+      <LoginLink>
+        <Button>SignIn</Button>
+      </LoginLink>
     </div>
   );
 }
